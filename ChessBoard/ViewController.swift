e98a4ff05986e63008ba0b89da8d188bb68b541f
Sam <20364339+shearos@users.noreply.github.com>
2023-01-14 10:01:34 +1000

    initial project

---
+++ ChessBoard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChessBoard
+//
+//  Created by Samuel Shearing on 14/1/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
